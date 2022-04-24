b83ae7504aec60cb7d21de0492e8c994c40e7856
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-23 22:10:55 -0300

    index.ts: "/usuarios"

--- index.ts
+++ index.ts
@@ -11,9 +11,22 @@ server.get('/', (req: Request, res: Response) => {
 
     return res.json({mensagem: 'Bem-vindo a API'});
 
-})
+});
+
+// GET 'usuarios'
+server.get('/usuarios', (req: Request, res: Response) => {
+    
+    return res.json([
+        {
+            name: 'Wilian'
+        },
+        {
+            name: 'Ana'
+        }
+    ])
+});
 
 // Servidor criado
 server.listen(port, () => {
-    console.log(`Server is running on ${host}${port}`);
+    console.log(`Server is running on ${host}${port}/`);
 })
