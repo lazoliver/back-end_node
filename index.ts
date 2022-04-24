c7718308ac878316c6bf49a2a66a5f0315c6b491
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-23 22:04:47 -0300

    index.ts: get "/"

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
+const host = 'http://localhost:'
 const port = 5000;
+
+// GET na rota '/'
+server.get('/', (req: Request, res: Response) => {
+    
+    // Todas as funcionalidades
+
+    return res.json({mensagem: 'Bem-vindo a API'});
+
+})
 
 // Servidor criado
 server.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+    console.log(`Server is running on ${host}${port}`);
 })
